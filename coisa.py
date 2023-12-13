28e958f42144394ef72de0b5289a9b310f7ccbae
FuscaRetratil <84513910+FuscaRetratil@users.noreply.github.com>
2023-12-13 11:19:33 -0300

    Create coisa.py

---
+++ coisa.py
@@ -0,0 +1,24 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager
+Window.size = (350, 550)
+
+class ChatBot(MDApp):
+
+    def mudar_tela(self, nome):
+        screen_manager.current = nome
+
+
+
+    def build(self):
+         global screen_manager
+         screen_manager = ScreenManager ()
+         screen_manager.add_widget(Builder.load_file("Main.kv"))
+         screen_manager.add_widget(Builder.load_file("Chats.kv"))
+         return screen_manager
+
+
+
+if __name__ == "__main__":
+    ChatBot().run()
