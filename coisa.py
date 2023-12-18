b8c1b71fe35cfb349922c36434e4e99d81c76dc7
IIDV <84513910+FuscaRetratil@users.noreply.github.com>
2023-12-18 14:32:34 -0300

    Update coisa.py

--- coisa.py
+++ coisa.py
@@ -2,6 +2,7 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.button.button import MDRaisedButton
 Window.size = (350, 550)
 
 class ChatBot(MDApp):
@@ -17,6 +18,8 @@ class ChatBot(MDApp):
          screen_manager.add_widget(Builder.load_file("Main.kv"))
          screen_manager.add_widget(Builder.load_file("Chats.kv"))
          return screen_manager
+    
+
     
     
 
