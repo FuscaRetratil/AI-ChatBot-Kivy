75e68b3d7729876e7d9c536e62a2c03764f25d8a
FuscaRetratil <84513910+FuscaRetratil@users.noreply.github.com>
2024-02-07 23:13:52 -0300

    Update coisa.py

--- coisa.py
+++ coisa.py
@@ -4,18 +4,18 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.uix.button.button import MDRaisedButton
 from kivy.core.text import LabelBase
+from openai import OpenAI
 import openai
+import os
 Window.size = (350, 550)
 
-chave_api = "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"
-openai.api_key = chave_api
+openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
 
 class ChatBot(MDApp):
 
     def mudar_tela(self, nome):
         screen_manager.current = nome
-
-
 
     def build(self):
          global screen_manager
@@ -25,15 +25,15 @@ class ChatBot(MDApp):
          return screen_manager
     
     def enviar_mensagem(mensagem):
-        resposta = openai.client.chat.completions.create (
+        resposta = client.chat.completions.create(
             model = "gpt-3.5-turbo",
             messages = [
                 {"role": "user", "content": mensagem}
             ],
         )
         return resposta ["choices"][0]["message"]
-
-
+    
+print(enviar_mensagem("Quando foi construída a estátua da liberdade?"))
 
 if __name__ == "__main__":
     LabelBase.register(name="Poppins", fn_regular="Poppins-Regular.ttf")
